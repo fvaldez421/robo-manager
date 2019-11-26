13a7410cd125a012be198c20a5382020fcacabe6
Frank Valdez <fvaldez421@gmail.com>
2019-11-25 23:53:01 -0800

    added state selectors and base action test

--- src/containers/Robots/index.js
+++ src/containers/Robots/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from "react-redux";
+import { injectReducer } from '../../utils/storeUtils';
+import * as actions from './actions';
+import reducer from './reducer';
+import { domain } from './constants';
+import { createKeySelector } from './selectors';
+import { createStructuredSelector } from 'reselect';
 
 
 const RobotsPage = props => {
-  console.log(props);
+  injectReducer(domain, reducer);
+  console.log(props)
+  const { status, activeRobots, getRobots } = props;
+  if (status === 'initial') getRobots('hello there');
   return (
     <div>
       These are robots
@@ -13,12 +23,17 @@ const RobotsPage = props => {
 
 
 RobotsPage.propTypes = {
+  status: PropTypes.string,
+  activeRobots: PropTypes.array,
+  getRobots: PropTypes.func,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => createStructuredSelector({
+  status: createKeySelector('status')(state),
+  activeRobots: createKeySelector('activeRobots')(state)
 });
 
-const mapDispatchToProps = {
-};
+// const mapDispatchToProps = mapDispatch(actions);
+const mapDispatchToProps = { ...actions }
 
 export default connect(mapStateToProps, mapDispatchToProps)(RobotsPage);
