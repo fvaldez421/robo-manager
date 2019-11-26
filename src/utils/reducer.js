91356bcc6c36733600617893e3a8c1479889760a
Frank Valdez <fvaldez421@gmail.com>
2019-11-25 23:52:30 -0800

    cleaned up redux code

--- src/utils/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import robotsReducer from '../containers/Robots/reducer';
-
-
-export default combineReducers ({
-  Robots: robotsReducer
-});
--- src/utils/request.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-
-export const get = () => {};
-export const put = () => {};
-export const post = () => {};
-export const remove = () => {};
