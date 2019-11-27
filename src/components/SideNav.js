1c958e63b506e2cd2724e439a0ff728007968ca4
Frank Valdez <fvaldez421@gmail.com>
2019-11-27 01:10:45 -0800

    updated styles and content for sidenav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { GridCol, GridContainer, FlexItem } from './StyleHelpers';
+import { lightGrey, darkGrey } from './Colors';
+import PepperIcon from '../images/Pepper_Icon.png';
 
+const SideNavWrapper = styled(FlexItem)`
+  background-color: ${lightGrey};
+  height: 100%;
+  max-width: 350px;
+  min-width: 250px;
+`;
 
-const NavContainer = styled.div`
+const PepperLogo = styled(Link)`
+  display: block;
+  height: 98px;
+  background-color: ${darkGrey};
+  background-image: url(${PepperIcon});
+  background-repeat: no-repeat;
+  background-position: 30px 55%;
+`;
+
+const ContentWrapper = styled.div`
+  padding: 0 15px;
 `;
 
 const SideNav = props => {
   const { location: { pathname ='' } } = window;
   console.log(pathname)
   return (
-    <NavContainer>
-      Side Nav
-    </NavContainer>
+    <SideNavWrapper grow={3}>  
+      <PepperLogo to='/dashboard' />
+      <ContentWrapper>
+      </ContentWrapper>
+    </SideNavWrapper>
   );
 };
 
