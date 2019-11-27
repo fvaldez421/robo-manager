cd1e25aa9eeefba13f369eda0a68cfb58d331915
Frank Valdez <fvaldez421@gmail.com>
2019-11-27 01:12:33 -0800

    added content to Robots page

--- src/containers/Robots/index.js
+++ src/containers/Robots/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import { createStructuredSelector } from 'reselect';
-
+import styled from 'styled-components';
 import { injectReducer } from '../../utils/storeUtils';
 import * as actions from './actions';
 import reducer from './reducer';
 import { domain } from './constants';
 import { createKeySelector } from './selectors';
+import { GridCol, PageHeader } from '../../components/StyleHelpers';
+
+const RobotsContainer = styled(GridCol)`
+`;
+
 
 
 const RobotsPage = props => {
@@ -15,9 +20,15 @@ const RobotsPage = props => {
   const { status, getRobots } = props;
   if (status === 'initial') getRobots('hello there');
   return (
-    <div>
-      These are robots
-    </div>
+    <RobotsContainer col={9}>
+    <PageHeader>
+      <h1>Robots</h1>
+    </PageHeader>
+      
+      <div>
+        hello there there are robots
+      </div>
+    </RobotsContainer>
   )
 };
 
