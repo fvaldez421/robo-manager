e073a6b92eec52713a0be7230c69d448c57f6300
Frank Valdez <fvaldez421@gmail.com>
2019-11-27 01:10:18 -0800

    added base dropdown component

---
+++ src/components/DropDown.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { darkGrey } from './Colors';
+
+
+const DropDownContainer = styled.div`
+  position: relative;
+  > span {
+    font-size: 13px;
+    font-weight: 600;
+    color: ${darkGrey};
+  }
+`;
+
+const Options = styled.div`
+  position: absolute;
+  display: ${({ open = false }) => open ? 'initial' : 'none'};
+`;
+
+const DropDownOption = ({ id, name, onSelect }) => (
+  <div onClick={() => onSelect(id)}>
+    {name}
+  </div>
+)
+
+
+export const DropDown = props => {
+  const { label = '', options = [], activeOption = null, onSelect } = props;
+  const [open = false, toggleOpen] = useState(false);
+  return (
+    <DropDownContainer>
+      <span>{label}</span>
+      
+      <Options open={open}>
+        {options.map(option => (
+          <DropDownOption {...option} onSelect={onSelect} />
+        ))}
+      </Options>
+    </DropDownContainer>
+  )
+}
